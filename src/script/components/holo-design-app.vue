102d246f6832a3364e7c33b0c3da0e7199836f1b
Matthew Nichols <matthew@matthew-nichols.com>
2020-03-08 16:42:43 +0100

    Encapsulated range input in component

--- src/script/components/holo-design-app.vue
+++ src/script/components/holo-design-app.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-      <input type="number" v-model.number="config.circleRadius" >
+      <numeric-range-input v-model.number="config.circleRadius" :min="50" :max="400" >Circle Radius</numeric-range-input>
       <button @click="rerunClick">Rerun</button>
     </div>
 </template>
@@ -10,6 +10,7 @@ import Vue from "vue";
 import { cloneDeep } from "lodash-es";
 
 import { createDesignRenderer, render, clearData, getConfig } from "../drawing/holo-design-setup";
+import NumericRangeInput from "./numeric-range-input.vue";
 
 //If the animation runs against Vue's reactive copy then it runs slowly. Do a deep clone to strip out the reactivity for rendering
 const createPlainCopyOfReactiveConfig = (reactiveConfig: any) => cloneDeep(reactiveConfig); 
@@ -19,6 +20,7 @@ export default Vue.extend({
   data() {
     return { config: getConfig() } 
   },
+  components: { NumericRangeInput },
   created() {
     createDesignRenderer(createPlainCopyOfReactiveConfig(this.config));
     render();
---
+++ src/script/components/numeric-range-input.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="range-input">
+        <label>
+            <div class="label-value">
+                <slot></slot>
+            </div>
+            <input type="range" ref="rangeInput" :value="value" @input="updateValue" :min="min" :max="max" />
+        </label>
+        <div class="input-value">
+            {{this.value}}
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: "NumericRangeInput",
+    props: {
+        value: Number,
+        min: Number,
+        max: Number
+    },
+    methods: {
+        updateValue(event: InputEvent) {
+            this.$emit("input", (this.$refs.rangeInput as HTMLInputElement).value);
+        }
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.range-input {
+    display: flex;
+
+    .input-value {
+        align-self: flex-end;
+        padding: 4px 0;
+        margin-left: 4px;
+        border: 1px solid #555;
+        box-shadow: 1px 1px 5px #555 inset;
+        color: #555;
+        width: 40px;
+        text-align: center;
+    }
+}
+</style>
