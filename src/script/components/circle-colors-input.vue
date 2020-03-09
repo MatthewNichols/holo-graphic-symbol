cf1a61c5e162a8a85aae2a660511b97c4bdce539
Matthew Nichols <matthew@matthew-nichols.com>
2020-03-09 11:34:57 +0100

    Got removing colors working, but the button is finiky about where you click

--- src/script/components/circle-colors-input.vue
+++ src/script/components/circle-colors-input.vue
@@ -3,6 +3,9 @@
         <h3>Circle Colors</h3>
         <div class="color" v-for="(color, index) in value" v-bind:key="index">
             <input type="color" ref="colorInput" :value="color" @change="updateValue" >
+            <button @click="removeItem" :value="color">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line pointer-events="none" x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+            </button>
         </div>
     </div>
 </template>
@@ -16,15 +19,36 @@ export default Vue.extend({
     },
     methods: {
         updateValue(event: InputEvent) {
-            const colors = (this.$refs.colorInput as HTMLInputElement[]).map((input) => input.value)
+            const colors = (this.$refs.colorInput as HTMLInputElement[]).map((input) => input.value);
             this.$emit("input", colors);
+        },
+        removeItem(event: InputEvent) {
+            const currentColors = (this.$refs.colorInput as HTMLInputElement[]).map((input) => input.value);
+            const colorToRemove = (event.target as HTMLButtonElement).value;
+            const newColors = currentColors.filter((color) => color !== colorToRemove);
+            this.$emit("input", newColors);
         }
     }
 })
 </script>
 
 <style lang="scss" scoped>
+@import "../../style/standard-elements";
+
 h3 {
     margin-top: 0;
+}
+
+.color {
+    display: flex;
+    align-items: center;
+
+    button {
+        @include button-base();
+
+        // svg {
+        //     pointer-events: none;
+        // }
+    }
 }
 </style>
