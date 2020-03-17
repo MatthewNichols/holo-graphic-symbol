da8bdb69d198a38fb942ef4fa4be5a51e86d6041
Matthew Nichols <matthew@matthew-nichols.com>
2020-03-17 17:28:00 +0100

    Improved open/close behavior. Made less ugly

--- src/script/components/ui-grouping.vue
+++ src/script/components/ui-grouping.vue
@@ -2,13 +2,13 @@
     <div class="ui-grouping">
         <h3>
             <slot name="name"></slot>
-            <button @click="toggleState">
+            <button @click="toggleState" title="Click to show/hide controls.">
                 <svg v-if="!showing" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
                 <svg v-if="showing" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>
             </button>
         </h3>
 
-        <transition name="fade">
+        <transition name="slide">
             <div class="ui-grouping-contents" v-if="showing">
                 <slot></slot>
             </div>
@@ -35,5 +35,21 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
+@import "../../style/standard-elements";
+
+button {
+    @include button-base();
+}
+
+.slide-enter-active, .slide-leave-active {
+  overflow: hidden;
+  transition: line-height .5s ease, opacity .25s;
+  line-height: 1;
+  opacity: 1;
+}
+.slide-enter, .slide-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  line-height: 0;
+  opacity: 0;
+}
 
 </style>
